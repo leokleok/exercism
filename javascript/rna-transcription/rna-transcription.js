653dd7982d7077fddbb59725286484b90e505e91
leok si <leoksi.koh@gmail.com>
2016-06-29 13:33:26 +0800

    complete rna transcription exercise

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,7 +1,22 @@
 var dnaTranscriber = function () {};
 
-dnaTranscriber.prototype.toRna = function () {
-  
-};
+
+//store into an object so you can assess the key value pairs
+dnaTranscriber.prototype.toRna = function (dna) {
+  var dnaRna = {
+    'G':'C',
+    'C':'G',
+    'T':'A',
+    'A':'U'
+  };
+
+//run a function that returns the obj key's value
+//mapping returns it into an array
+//join() method joins all elements of an array into a string.
+  function translate(nucleotide) {
+         return dnaRna[nucleotide];
+     }
+     return dna.split().map(translate).join();
+ };
 
 module.exports = dnaTranscriber;
