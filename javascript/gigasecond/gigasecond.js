91d07601eae040ca4ea2364ab8780d7074c90194
leok si <leoksi.koh@gmail.com>
2016-07-07 17:29:21 +0800

    gigasecond

---
+++ javascript/gigasecond/gigasecond.js
@@ -0,0 +1,17 @@
+var Gigasecond = function () {};
+
+function Gigasecond (date) {
+  this.startDate = date;
+
+  gs.prototype.date = function () {
+    var startTime = this.startDate.getTime();
+    var endDate = new Date(startTime + 1000000000000);
+    endDate.setHours(0);
+    endDate.setSeconds(0);
+    endDate.setMinutes(0);
+    return endDate;
+};
+
+}
+
+module.exports = Gigasecond;
